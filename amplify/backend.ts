e6a4bc60ed479c794fb620a82cbb9f41a97afb91
nakamura0736 <tel07360102@gmail.com>
2024-12-03 18:09:30 +0900

    add auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { helloWorld } from './function/hello-world/resource';
+import { auth } from './auth/resource';
 
 const backend = defineBackend({
   helloWorld,
@@ -9,4 +10,8 @@ backend.addOutput({
   custom: {
     helloWorldFunctionName: backend.helloWorld.resources.lambda.functionName,
   },
+});
+
+defineBackend({
+  auth,
 });
