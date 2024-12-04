f75c5c301e53eff254a6b9aa515c537e79cdafad
nakamura0736 <tel07360102@gmail.com>
2024-12-04 10:37:44 +0900

    test

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,17 +2,13 @@ import { defineBackend } from '@aws-amplify/backend';
 import { helloWorld } from './function/hello-world/resource';
 // import { auth } from './auth/resource';
 
-// const backend = defineBackend({
-//   helloWorld,
-// });
-
-// backend.addOutput({
-//   custom: {
-//     helloWorldFunctionName: backend.helloWorld.resources.lambda.functionName,
-//   },
-// });
-
-defineBackend({
-  // auth,
+const backend = defineBackend({
   helloWorld,
+  auth,
+});
+
+backend.addOutput({
+  custom: {
+    helloWorldFunctionName: backend.helloWorld.resources.lambda.functionName,
+  },
 });
