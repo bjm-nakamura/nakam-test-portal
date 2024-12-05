b24aa3ebdf66d59eb999d6234d0a9f54acca0302
nakamura0736 <tel07360102@gmail.com>
2024-12-05 23:26:10 +0900

    a

--- amplify/function/hello-world/resource.ts
+++ amplify/function/hello-world/resource.ts
@@ -1,7 +1,8 @@
 import { defineFunction } from '@aws-amplify/backend';
 
 export const helloWorld = defineFunction({
-    runtime: 'nodejs14.x', 
+    // runtime: 'nodejs14.x', 
+    runtime: 20, 
     name: 'hello-world',
     entry: './handler.ts',
 })
